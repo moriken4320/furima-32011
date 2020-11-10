c6c138cbeaee9ad3734c57a22e4ed746ea97478c
moriken4320 <moriken.4320@gmail.com>
2020-11-10 14:34:14 +0900

    複数回使用する正規表現を変数に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  half_width_alphanumeric = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  full_width_character = /\A[ぁ-んァ-ン一-龥]/
+  full_width_katakana_character = /\A[ァ-ヶー－]+\z/
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Input half-width alphanumeric characters mix.' }
+  validates :password, format: { with: half_width_alphanumeric, message: 'is invalid. Input half-width alphanumeric characters mix.' }
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :last_name, format: { with: full_width_character, message: 'is invalid. Input full-width characters.' }
+    validates :first_name, format: { with: full_width_character, message: 'is invalid. Input full-width characters.' }
+    validates :last_name_kana, format: { with: full_width_katakana_character, message: 'is invalid. Input full-width katakana characters.' }
+    validates :first_name_kana, format: { with: full_width_katakana_character, message: 'is invalid. Input full-width katakana characters.' }
     validates :birthday
   end
 end
