b6595adac576a53360ae13d4e05ba8570164d92e
moriken4320 <moriken.4320@gmail.com>
2020-11-10 19:00:32 +0900

    item_controllerのnew,createアクションを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,23 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
   end
   
   def create
-    
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :quality_status_id, :delivery_burden_id, :ship_from_location_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+  
   
 end
