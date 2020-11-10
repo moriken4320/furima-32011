8b89a199785825e119b391b284414b8cb3b5804d
moriken4320 <moriken.4320@gmail.com>
2020-11-10 15:23:04 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  [:name, :description, :category_id, :quality_status_id, :delivery_burden_id, :ship_from_location_id, :delivery_time_id, :price,:user, :image].each do |v|
+    validates v, presence: true
+  end
+
+  belongs_to :user
+  has_one_attached :image
 end
