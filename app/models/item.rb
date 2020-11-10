7ce63e13bc02e76bc8cf793536629c274299a1b3
moriken4320 <moriken.4320@gmail.com>
2020-11-10 20:50:35 +0900

    itemモデルのバリデーションを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,18 @@
 class Item < ApplicationRecord
-
   with_options presence: true do
-    validates :name
-    validates :description
-    with_options numericality: { other_than: 0 } do
-      validates :category_id
-      validates :quality_status_id
-      validates :delivery_burden_id
-      validates :ship_from_location_id
-      validates :delivery_time_id
+    [:name, :description, :image].each do |v|
+      validates v
     end
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-    validates :user
-    validates :image
   end
+
+  with_options numericality: { other_than: 0 } do
+    [:category_id, :quality_status_id, :delivery_burden_id, :ship_from_location_id, :delivery_time_id].each do |v|
+      validates v
+    end
+  end
+  
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
 
   belongs_to :user
   has_one_attached :image
