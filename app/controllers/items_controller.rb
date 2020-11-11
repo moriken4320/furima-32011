5e67914864936a3162eed2c0c49c25a6df897464
moriken4320 <moriken.4320@gmail.com>
2020-11-11 17:42:47 +0900

    不要な記述を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,6 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless user_signed_in? && current_user.id == @item.user.id
+    redirect_to root_path unless current_user.id == @item.user.id
   end
 end
