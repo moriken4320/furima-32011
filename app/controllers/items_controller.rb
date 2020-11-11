388215eae494d361fde23c9529797a0347120e6b
moriken4320 <moriken.4320@gmail.com>
2020-11-11 16:53:30 +0900

    itemsコントローラーを編集し、edit,updateアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at desc')
@@ -20,12 +21,34 @@ class ItemsController < ApplicationController
   end
 
   def show
+
+  end
+
+  def edit
+    move_to_index(@item)
+  end
+  
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+  
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :quality_status_id, :delivery_burden_id, :ship_from_location_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
     @item = Item.find(params[:id])
   end
 
-  private
-
-  def item_params
-    params.require(:item).permit(:name, :description, :category_id, :quality_status_id, :delivery_burden_id, :ship_from_location_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
+  def move_to_index(item)
+    unless user_signed_in? && current_user.id == item.user.id
+      redirect_to root_path
+    end
   end
 end
