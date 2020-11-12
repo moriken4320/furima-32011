4d718e94a9190cc320aaebdfc201c5cf67af0338
moriken4320 <moriken.4320@gmail.com>
2020-11-12 21:04:09 +0900

    商品の出品者ではない、かつ、商品が売れていない場合以外はトップページへ遷移するように設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index]
 
   def index
     @purchase_address = PurchaseAddress.new
@@ -34,5 +35,12 @@ class PurchasesController < ApplicationController
       currency: 'jpy'
     )
   end
+
+  def move_to_index
+    unless current_user.id != @item.user.id && Purchase.where(item_id: @item.id).count <= 0
+      redirect_to root_path
+    end
+  end
+  
   
 end
