54658191f32959e3d642d4e25dc791f45eb43b67
moriken4320 <moriken.4320@gmail.com>
2020-11-12 15:46:31 +0900

    purchase、address、purchase_addressモデルの単体テストコード作成

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @purchase = FactoryBot.build(:purchase)
+  end
+
+  context "購入情報が保存可能" do
+    it "user,itemと紐づいていれば保存できる" do
+      expect(@purchase).to be_valid 
+    end
+  end
+  
+  context "購入情報が保存不可能" do
+    it "userが紐づいていなければ保存できない" do
+      @purchase.user = nil
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include("User must exist") 
+    end
+    it "itemが紐づいていなければ保存できない" do
+      @purchase.item = nil
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include("Item must exist") 
+    end
+  end
+  
 end
