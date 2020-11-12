0fed037c063d10b350587657acf3f332399ba714
moriken4320 <moriken.4320@gmail.com>
2020-11-12 19:29:50 +0900

    purchase_addressモデルにもtokenを追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,11 +1,11 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly" } #ハイフン必須
     validates :prefecture_id, numericality: {other_than: 0, message: "Select"} #0以外
-    [:municipality, :house_number].each do |v|
+    [:municipality, :house_number, :token].each do |v|
       validates v
     end
     validates :phone_number, length: {maximum: 11}, numericality: { only_integer: true, message: "Input only number" } #11桁以内,自然数オンリー
@@ -13,7 +13,7 @@ class PurchaseAddress
 
 
   def save
-    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id, token: token)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
