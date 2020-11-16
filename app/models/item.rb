24bbdf59d9e6b39fa68876c86f7a7c01c14be10c
moriken4320 <moriken.4320@gmail.com>
2020-11-16 09:46:59 +0900

    エラー文を日本語対応

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,9 @@ class Item < ApplicationRecord
       validates v
     end
 
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は300~9999999円の範囲で入力してください' }
 
-    with_options numericality: { other_than: 0, message: 'Select' } do
+    with_options numericality: { other_than: 0, message: 'を選択してください' } do
       [:category_id, :quality_status_id, :delivery_burden_id, :ship_from_location_id, :delivery_time_id].each do |v|
         validates v
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, format: { with: half_width_alphanumeric, message: 'is invalid. Input half-width alphanumeric characters mix.' }
+  validates :password, format: { with: half_width_alphanumeric, message: 'は半角英数字で入力してください' }
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: full_width_character, message: 'is invalid. Input full-width characters.' }
-    validates :first_name, format: { with: full_width_character, message: 'is invalid. Input full-width characters.' }
-    validates :last_name_kana, format: { with: full_width_katakana_character, message: 'is invalid. Input full-width katakana characters.' }
-    validates :first_name_kana, format: { with: full_width_katakana_character, message: 'is invalid. Input full-width katakana characters.' }
+    validates :last_name, format: { with: full_width_character, message: 'は全角で入力してください' }
+    validates :first_name, format: { with: full_width_character, message: 'は全角で入力してください' }
+    validates :last_name_kana, format: { with: full_width_katakana_character, message: 'は全角カナで入力してください' }
+    validates :first_name_kana, format: { with: full_width_katakana_character, message: 'は全角カナで入力してください' }
     validates :birthday
   end
 
